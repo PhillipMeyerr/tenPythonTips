4ae01cd167b092f3056ad9fccefc0a7d695946cc
Phillip Meyer <philllipmeyer6291@gmail.com>
2025-02-27 18:06:24 +0200

    For loops 4

--- forLoop2.py
+++ forLoop2.py
@@ -16,7 +16,7 @@ for number in range(50):
 #print(even_nums1)
 
 #Best Practice 
-
-evens = [number for number in range(1,51) if number % 2 == 0]
+#Ngl pretty cool!
+evens = [number for number in range(0,51) if number % 2 == 0]
 
 print(evens)
---
+++ forLoops4.py
@@ -0,0 +1,17 @@
+# Looking a nested list comprehension
+# This is the scrub way of doing things
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+flattend = []
+
+for row in matrix:
+    for num in row:
+        flattend.append(num)
+        continue
+#print(flattend)
+###
+### Baller way to do this
+
+flattend = []
+flattend = [num for row in matrix for num in row]
+
+print(flattend)
